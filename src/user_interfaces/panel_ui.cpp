b0173080ef24a61c229fccc93c2d76914041438d
Steini1998 <s4nsstei@uni-trier.de>
2025-01-03 14:09:10 +0100

    Updated Panel_UI as it now is independent of GraphWindow and can be moved. Still didn't find a method to change and update the panel-positioning.

--- src/user_interfaces/panel_ui.cpp
+++ src/user_interfaces/panel_ui.cpp
@@ -7,11 +7,13 @@ panel_ui::panel_ui(leda::GraphWin& gw) : m_graphwin{gw}, m_panel{"Walk through s
 }
 
 int panel_ui::nextStep() {
-    return m_graphwin.open_panel(m_panel);
+    return m_panel.open(); // m_graphwin.open_panel(m_panel);
 }
 
 void panel_ui::configurePanel() {
-    m_panel.button("Stop", 0);
+	m_panel.buttons_per_line(1);
+	
+	m_panel.button("Stop", 0);
     m_panel.button("Move Step Backward", 1);
 	m_panel.button("Move Step Forward", 2);
 }
